e76c8dfc8c3b43c187a7870d606cc6e4de0f7dfd
shengwenqi <1584674061@qq.com>
2018-01-26 09:40:26 +0800

    it's a java file

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car();
+        c1.color="red";
+        c1.num=4;
+        c1.run();
+        c2.run();
+    }
+}
